c94a6bc3f2786ebf45589221db4270226dee3e3d
Doomsquard <zhilinsky99@gmail.com>
2021-05-04 16:16:40 +0300

    initial commit

---
+++ habit-tracker/src/components/StartComponent.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h1>HABIT-TRACKER initial</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'StartComponent',
+	}
+</script>
---
+++ habit-tracker/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<StartComponent />
+	</div>
+</template>
+
+<script>
+	// @ is an alias to /src
+	import StartComponent from '@/components/StartComponent.vue'
+
+	export default {
+		name: 'Home',
+		components: {
+			StartComponent,
+		},
+	}
+</script>
